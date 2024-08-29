172048c37997ced88c345432cb0702e4d4f8c46b
SilenceFox <cristianplaybr@gmail.com>
2024-08-29 12:56:48 -0300

    Make Fudge Dice Endpoint Work

--- models/RollingRequestModule.cs
+++ models/RollingRequestModule.cs
@@ -1,30 +1,31 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using System.Text.Json;
 
 namespace Requests.Utility
 {
     public static class RollingRequestModule
     {
-	public static void AddRequests(WebApplication webApplication)
-	{
+        public static void AddRequests(WebApplication webApplication)
+        {
             webApplication.MapPost(RollDiceRequest.EndPoint, RollDiceRequest.Respond);
 
             webApplication.MapPost(RollFudgeRequest.EndPoint, RollFudgeRequest.Respond);
         }
     }
-    
+
     [Serializable]
     public static class RollDiceRequest
     {
         public const String EndPoint = "/d20";
 
-	public static int Respond(HttpContext context)
-	{
+        public static int Respond(HttpContext context)
+        {
             Random random = new Random();
 
             String? sidesString = context.Request.Query["sides"];
-	    if (sidesString == null)
-	    {
+            if (sidesString == null)
+            {
                 sidesString = "20";
             }
 
@@ -41,31 +42,34 @@ namespace Requests.Utility
         public const String EndPoint = "/fudge";
 
 	[Serializable]
-	public struct FudgeResult
-	{
-            public List<int> Rolls;
-            public int Result;
+        public class FudgeResult
+        {
+            public List<int> Rolls {get; set;} = new List<int>();
+            public int Result {get; set;}
         }
 
-	public static FudgeResult Respond(HttpContext context)
-	{
-            FudgeResult result = new FudgeResult();
-            result.Rolls = new List<int>();
+        public static string Respond(HttpContext context)
+        {
+            FudgeResult result = new FudgeResult
+            {
+                Rolls = new List<int>()
+            };
 
             Random random = new Random();
 
-            for (int i = 0; i < 3; i++)
-	    {
-		int randomInt = random.Next(-1, 1);
-		result.Rolls.Add(randomInt);
-	    }
-
-            for (int i = 0; i < 3; i++)
-	    {
-                result.Result += result.Rolls[i];
+            for (int i = 0; i < 4; i++)
+            {
+                int randomInt = random.Next(-1, 2);
+                result.Rolls.Add(randomInt);
             }
 
-            return result;
+            result.Result = result.Rolls.Sum();
+
+            string jsonResult = JsonSerializer.Serialize(result);
+            context.Response.ContentType = "application/json";
+
+            return jsonResult;
+            // context.Response.WriteAsync(jsonResult).Wait();
         }
 
     }
